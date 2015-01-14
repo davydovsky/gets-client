795530fa06acc49efbe7b38ec5da4d8115b5aeee
davydovsky <davydovs59@gmail.com>
2015-01-14 12:05:02 +0300

    fix indentations

--- gets-web-client/app/app.js
+++ gets-web-client/app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('geTSWebClient', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'geTSWebClient.view1',
+  'geTSWebClient.view2',
+  'geTSWebClient.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- gets-web-client/app/components/version/version.js
+++ gets-web-client/app/components/version/version.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.version', [
-  'myApp.version.interpolate-filter',
-  'myApp.version.version-directive'
+angular.module('geTSWebClient.version', [
+    'geTSWebClient.version.interpolate-filter',
+    'geTSWebClient.version.version-directive'
 ])
 
 .value('version', '0.0.1');
